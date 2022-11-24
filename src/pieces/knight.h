3925a3165fdcdaa5a3430d349e0423dc1466a238
karmdesai <karmdesai18@gmail.com>
2022-11-24 00:06:35 -0500

    implement Knight piece

--- src/pieces/knight.h
+++ src/pieces/knight.h
@@ -3,4 +3,12 @@
 
 #include "piece.h"
 
+class Knight : public Piece {
+  public:
+    Knight(char name, char color);
+    ~Knight() = default;
+
+    void getAllPossibleMoves(std::pair<char, int> currentPosition) override;
+};
+
 #endif
