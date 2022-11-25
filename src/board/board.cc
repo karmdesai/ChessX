3c53ccaf64960a5df54cf565ca6b0b81d228d73a
Ansh Chaturvedi <a4chatur@uwaterloo.ca>
2022-11-24 19:49:36 -0500

    formatting

--- src/board/board.cc
+++ src/board/board.cc
@@ -13,76 +13,75 @@
 
 /* Board class */
 Board::Board(bool customSetup) {
-    // initialize board to nullptr
-    for (int x = 0; x < 8; x++) {
-        for (int y = 0; y < 8; y++) {
-            currentBoard[x][y] = nullptr;
-        }
+  // initialize board to nullptr
+  for (int x = 0; x < 8; x++) {
+    for (int y = 0; y < 8; y++) {
+      currentBoard[x][y] = nullptr;
+    }
+  }
+
+  if (customSetup == false) {
+    // Initialize white pieces
+    currentBoard[0][0] = new Rook('R', 'w', true);
+    currentBoard[1][0] = new Knight('N', 'w');
+    currentBoard[2][0] = new Bishop('B', 'w');
+    currentBoard[3][0] = new Queen('Q', 'w');
+    currentBoard[4][0] = new King('K', 'w', true);
+    currentBoard[5][0] = new Bishop('B', 'w');
+    currentBoard[6][0] = new Knight('N', 'w');
+    currentBoard[7][0] = new Rook('R', 'w', true);
+
+    for (int i = 0; i < 8; i++) {
+      currentBoard[i][1] = new Pawn('P', 'w', true);
     }
 
-    if (customSetup == false) {
+    // Initialize black pieces
+    currentBoard[0][7] = new Rook('r', 'b', true);
+    currentBoard[1][7] = new Knight('n', 'b');
+    currentBoard[2][7] = new Bishop('b', 'b');
+    currentBoard[3][7] = new Queen('q', 'b');
+    currentBoard[4][7] = new King('k', 'b', true);
+    currentBoard[5][7] = new Bishop('b', 'b');
+    currentBoard[6][7] = new Knight('n', 'b');
+    currentBoard[7][7] = new Rook('r', 'b', true);
 
-        // Initialize white pieces
-        currentBoard[0][0] = new Rook('R', 'w', true);
-        currentBoard[1][0] = new Knight('N', 'w');
-        currentBoard[2][0] = new Bishop('B', 'w');
-        currentBoard[3][0] = new Queen('Q', 'w');
-        currentBoard[4][0] = new King('K', 'w', true);
-        currentBoard[5][0] = new Bishop('B', 'w');
-        currentBoard[6][0] = new Knight('N', 'w');
-        currentBoard[7][0] = new Rook('R', 'w', true);
-
-        for (int i = 0; i < 8; i++) {
-            currentBoard[i][1] = new Pawn('P', 'w', true);
-        }
-
-        // Initialize black pieces
-        currentBoard[0][7] = new Rook('r', 'b', true);
-        currentBoard[1][7] = new Knight('n', 'b');
-        currentBoard[2][7] = new Bishop('b', 'b');
-        currentBoard[3][7] = new Queen('q', 'b');
-        currentBoard[4][7] = new King('k', 'b', true);
-        currentBoard[5][7] = new Bishop('b', 'b');
-        currentBoard[6][7] = new Knight('n', 'b');
-        currentBoard[7][7] = new Rook('r', 'b', true);
-
-        for (int i = 0; i < 8; i++) {
-            currentBoard[i][6] = new Pawn('p', 'b', true);
-        }
+    for (int i = 0; i < 8; i++) {
+      currentBoard[i][6] = new Pawn('p', 'b', true);
     }
+  }
 }
 
 // Overload the << operator for the Board class
 std::ostream &operator<<(std::ostream &out, const Board *myBoard) {
-    for (int y = 7; y >= 0; y--) {
-        out << y + 1 << " ";
+  for (int y = 7; y >= 0; y--) {
+    out << y + 1 << " ";
 
-        for (int x = 0; x < 8; x++) {
-            Piece *currentSpace = myBoard->currentBoard[x][y];
+    for (int x = 0; x < 8; x++) {
+      Piece *currentSpace = myBoard->currentBoard[x][y];
 
-            if (currentSpace != nullptr) {
-                out << currentSpace->getName();
-            } else {
-                // a even sum of coordinates is a black square
-                if ((x + y) % 2 == 0) {
-                    out << "_";
-                }
-                // an odd sum of coordinates is a white square
-                else {
-                    out << " ";
-                }
-            }
+      if (currentSpace != nullptr) {
+        out << currentSpace->getName();
+      } else {
+        // a even sum of coordinates is a black square
+        if ((x + y) % 2 == 0) {
+          out << "_";
         }
-
-        out << std::endl;
+        // an odd sum of coordinates is a white square
+        else {
+          out << " ";
+        }
+      }
     }
 
-    // print x-axis as letters
     out << std::endl;
-    out << "  abcdefgh";
-    out << std::endl;
+  }
 
-    return out;
+  // print x-axis as letters
+  out << std::endl;
+  out << "  abcdefgh";
+  out << std::endl;
+
+  return out;
 }
 
 // convertAlphaToNum() converts a character ranging from a to h
@@ -91,69 +90,70 @@ std::ostream &operator<<(std::ostream &out, const Board *myBoard) {
 //  - alpha is a character ranging from a to h.
 // time: O(1)
 int Board::convertAlphaToNum(char alpha) {
-    if (alpha >= 'a' && alpha <= 'h') {
-        return alpha - 'a';
-    } else {
-        return -1;
-    }
+  if (alpha >= 'a' && alpha <= 'h') {
+    return alpha - 'a';
+  } else {
+    return -1;
+  }
 }
 
 Piece *Board::pieceAtPosition(std::pair<char, int> position) {
-    int x = convertAlphaToNum(position.first);
-    int y = position.second;
+  int x = convertAlphaToNum(position.first);
+  int y = position.second;
 
-    /* Check if position is out of bounds first?
+  /* Check if position is out of bounds first?
 
-    if (x == -1 || y < 1 || y > 8) {
-        return false;
-    }
-    */
+  if (x == -1 || y < 1 || y > 8) {
+      return false;
+  }
+  */
 
-    // input is always from 1 to 8 but array indexing is from 0 to 7.
-    x -= 1;
-    y -= 1;
+  // input is always from 1 to 8 but array indexing is from 0 to 7.
+  x -= 1;
+  y -= 1;
 
-    return currentBoard[x][y];
+  return currentBoard[x][y];
 }
 
 void Board::parsePossibleMoves(Piece &piece, std::pair<char, int> position) {
-    if (piece.getName() == 'p' || piece.getName() == 'P') {
-        // Pawn
-    } else if (piece.getName() == 'r' || piece.getName() == 'R') {
-        // Rook
-    } else if (piece.getName() == 'n' || piece.getName() == 'N') {
-        parsePossibleMovesKnight(piece, position);
-    } else if (piece.getName() == 'b' || piece.getName() == 'B') {
-        // Bishop
-    } else if (piece.getName() == 'q' || piece.getName() == 'Q') {
-        // Queen
-    } else if (piece.getName() == 'k' || piece.getName() == 'K') {
-        // King
-    }
+  if (piece.getName() == 'p' || piece.getName() == 'P') {
+    // Pawn
+  } else if (piece.getName() == 'r' || piece.getName() == 'R') {
+    // Rook
+  } else if (piece.getName() == 'n' || piece.getName() == 'N') {
+    parsePossibleMovesKnight(piece, position);
+  } else if (piece.getName() == 'b' || piece.getName() == 'B') {
+    // Bishop
+  } else if (piece.getName() == 'q' || piece.getName() == 'Q') {
+    // Queen
+  } else if (piece.getName() == 'k' || piece.getName() == 'K') {
+    // King
+  }
 }
 
 void Board::parsePossibleMovesKnight(Piece &knight,
                                      std::pair<char, int> position) {
+  // initialize iterators
+  auto it = knight.allPossibleMoves.begin();
+  auto end = knight.allPossibleMoves.end();
 
-	// initialize iterators
-	auto it = knight.allPossibleMoves.begin();
-	auto end = knight.allPossibleMoves.end();
+  while (it != end) {
+    Piece *currentSpace = this->pieceAtPosition(*it);
+    bool erased = false;
 
-	while (it != end) {
-		Piece *currentSpace = this->pieceAtPosition(*it);
-		bool erased = false;
+    // if piece is occupied and of same colour, we remove from possible moves
+    if (currentSpace != nullptr) {
+      if (currentSpace->getColor() == knight.getColor()) {
+        it = knight.allPossibleMoves.erase(it);
+        erased = true;
+        if (it == end) {
+          break;
+        }
+      }
+    }
 
-		// if piece is occupied and of same colour, we remove from possible moves
-		if (currentSpace != nullptr) {
-			if (currentSpace->getColor() == knight.getColor()) {
-				it = knight.allPossibleMoves.erase(it);
-				erased = true;
-				if (it == end) {
-					break;
-				}
-			} 
-		}
-
-		if (!erased) { ++it; }
-	}
+    if (!erased) {
+      ++it;
+    }
+  }
 }
