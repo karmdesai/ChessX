b1d48366048b99817585a1b3208deda0c125b666
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2022-11-26 18:44:53 -0500

    added rules to Makefile and included files in driver

--- src/computer/abstractPlayer.h
+++ src/computer/abstractPlayer.h
@@ -1,5 +1,20 @@
 #ifndef ABSTRACT_PLAYER_H
 #define ABSTRACT_PLAYER_H
 
+class Board;
+
+class AbstractPlayer {
+  protected:
+    char playerColor;
+    Board *board;
+  
+  public:
+    AbstractPlayer(char playerColor, Board *board);
+    virtual ~AbstractPlayer();
+    
+    // Getters
+    char getPlayerColor();
+    Board *getBoard();
+};
 
 #endif
