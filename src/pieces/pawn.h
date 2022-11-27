f6c8af6eea19c46eff2e931ea2c6841aa366eaff
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2022-11-27 12:35:12 -0500

    initial implementation for computer level 2

--- src/pieces/pawn.h
+++ src/pieces/pawn.h
@@ -4,13 +4,13 @@
 #include "piece.h"
 
 class Pawn : public Piece {
-private:
+ private:
   /* We use this to determine whether or not
       the pawn can move two spaces, and by
       extension, can be en passant captured. */
   bool inStartingPosition;
 
-public:
+ public:
   Pawn(char name, char color, bool inStartingPosition);
   ~Pawn() = default;
 
