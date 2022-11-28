10f8ccf73185fead7c2ded9154ee449ee34d51de
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2022-11-27 19:26:12 -0500

    changed driver code to determine custom setup as a command line flag, tested computer on any random config of board

--- src/driver.cc
+++ src/driver.cc
@@ -17,7 +17,7 @@
 #include "pieces/queen.h"
 #include "pieces/rook.h"
 
-int main() {
+int main(int argc, char *argv[]) {
   std::cout << "**** WELCOME TO CHESS ****" << std::endl;
 
   std::cout << "Please enter the setup command immediately if you would like "
@@ -27,14 +27,23 @@ int main() {
             << std::endl;
   std::cout << std::endl;
 
+  // check for whether user wants default setup or custom setup through commandline flag
+  bool customSetup = false;
+  if (argc > 1) {
+    std::string flag = argv[1];
+    if (flag == "-c") {
+      customSetup = true;
+    }
+  }
+
   // you can only setup on the very first command for now.
-  std::string firstCommand;
-  std::cin >> firstCommand;
+  // std::string firstCommand;
+  // std::cin >> firstCommand;
 
   Board *b = new Board();
 
   /* Start Board Setup */
-  if (firstCommand == "setup") {
+  if (customSetup) {
     std::cout << b << std::endl;
 
     std::string setupCommand;
@@ -158,140 +167,140 @@ int main() {
   std::cout << b << std::endl;
   /* End Board Setup */
 
-  std::cout << "Enter a piece followed by current position to get all possible "
-               "moves: "
-            << std::endl;
-  std::cout << "e.g. 'R a1' for white rook currently at a1" << std::endl;
-  std::cout << "e.g. 'b h4' for black bishop currently at h4" << std::endl;
+  // std::cout << "Enter a piece followed by current position to get all possible "
+  //              "moves: "
+  //           << std::endl;
+  // std::cout << "e.g. 'R a1' for white rook currently at a1" << std::endl;
+  // std::cout << "e.g. 'b h4' for black bishop currently at h4" << std::endl;
 
-  Piece *p = nullptr;
+  // Piece *p = nullptr;
 
-  char piece;
-  char x;
-  int y;
+  // char piece;
+  // char x;
+  // int y;
 
-  std::cin >> piece >> x >> y;
+  // std::cin >> piece >> x >> y;
 
-  if (x < 'a' || x > 'h' || y < 1 || y > 8) {
-    std::cout << "Invalid position" << std::endl;
-    return 1;
-  }
+  // if (x < 'a' || x > 'h' || y < 1 || y > 8) {
+  //   std::cout << "Invalid position" << std::endl;
+  //   return 1;
+  // }
 
-  std::pair<char, int> position = std::make_pair(x, y);
+  // std::pair<char, int> position = std::make_pair(x, y);
 
-  if (piece == 'p') {
-    bool inStartingPosition = true;
+  // if (piece == 'p') {
+  //   bool inStartingPosition = true;
 
-    if (y != 7) {
-      inStartingPosition = false;
-    }
+  //   if (y != 7) {
+  //     inStartingPosition = false;
+  //   }
 
-    p = new Pawn(piece, 'b', inStartingPosition);
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p = new Pawn(piece, 'b', inStartingPosition);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'r') {
-    bool inStartingPosition = false;
+  // } else if (piece == 'r') {
+  //   bool inStartingPosition = false;
 
-    if ((x == 'a' && y == 8) || (x == 'h' && y == 8)) {
-      inStartingPosition = true;
-    }
+  //   if ((x == 'a' && y == 8) || (x == 'h' && y == 8)) {
+  //     inStartingPosition = true;
+  //   }
 
-    p = new Rook(piece, 'w', inStartingPosition);
+  //   p = new Rook(piece, 'w', inStartingPosition);
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'n') {
-    p = new Knight(piece, 'b');
+  // } else if (piece == 'n') {
+  //   p = new Knight(piece, 'b');
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'b') {
-    p = new Bishop(piece, 'b');
+  // } else if (piece == 'b') {
+  //   p = new Bishop(piece, 'b');
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'q') {
-    p = new Queen(piece, 'b');
+  // } else if (piece == 'q') {
+  //   p = new Queen(piece, 'b');
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'k') {
-    bool inStartingPosition = true;
+  // } else if (piece == 'k') {
+  //   bool inStartingPosition = true;
 
-    if (x != 'e' || y != 8) {
-      inStartingPosition = false;
-    }
+  //   if (x != 'e' || y != 8) {
+  //     inStartingPosition = false;
+  //   }
 
-    p = new King(piece, 'b', inStartingPosition);
-    p->getAllPossibleMoves(position);
+  //   p = new King(piece, 'b', inStartingPosition);
+  //   p->getAllPossibleMoves(position);
 
-  } else if (piece == 'P') {
-    bool inStartingPosition = true;
+  // } else if (piece == 'P') {
+  //   bool inStartingPosition = true;
 
-    if (y != 2) {
-      inStartingPosition = false;
-    }
+  //   if (y != 2) {
+  //     inStartingPosition = false;
+  //   }
 
-    p = new Pawn(piece, 'w', inStartingPosition);
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p = new Pawn(piece, 'w', inStartingPosition);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'R') {
-    bool inStartingPosition = false;
+  // } else if (piece == 'R') {
+  //   bool inStartingPosition = false;
 
-    if ((x == 'a' && y == 1) || (x == 'h' && y == 1)) {
-      inStartingPosition = true;
-    }
+  //   if ((x == 'a' && y == 1) || (x == 'h' && y == 1)) {
+  //     inStartingPosition = true;
+  //   }
 
-    p = new Rook(piece, 'w', inStartingPosition);
+  //   p = new Rook(piece, 'w', inStartingPosition);
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'N') {
-    p = new Knight(piece, 'w');
+  // } else if (piece == 'N') {
+  //   p = new Knight(piece, 'w');
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'B') {
-    p = new Bishop(piece, 'w');
+  // } else if (piece == 'B') {
+  //   p = new Bishop(piece, 'w');
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'Q') {
-    p = new Queen(piece, 'w');
+  // } else if (piece == 'Q') {
+  //   p = new Queen(piece, 'w');
 
-    p->getAllPossibleMoves(position);
-    b->parsePossibleMoves(*p, position);
+  //   p->getAllPossibleMoves(position);
+  //   b->parsePossibleMoves(*p, position);
 
-  } else if (piece == 'K') {
-    bool inStartingPosition = true;
+  // } else if (piece == 'K') {
+  //   bool inStartingPosition = true;
 
-    if (x != 'e' || y != 1) {
-      inStartingPosition = false;
-    }
+  //   if (x != 'e' || y != 1) {
+  //     inStartingPosition = false;
+  //   }
 
-    p = new King(piece, 'w', inStartingPosition);
-    p->getAllPossibleMoves(position);
+  //   p = new King(piece, 'w', inStartingPosition);
+  //   p->getAllPossibleMoves(position);
 
-  } else {
-    std::cout << "Invalid piece" << std::endl;
-  }
+  // } else {
+  //   std::cout << "Invalid piece" << std::endl;
+  // }
 
-  if (p != nullptr) {
-    for (auto move : p->allPossibleMoves) {
-      std::cout << move.first << move.second << ", ";
-    }
+  // if (p != nullptr) {
+  //   for (auto move : p->allPossibleMoves) {
+  //     std::cout << move.first << move.second << ", ";
+  //   }
 
-    std::cout << std::endl;
-  }
+  //   std::cout << std::endl;
+  // }
 
   /*
   Driver code for testing moving with the computer:
@@ -317,6 +326,8 @@ int main() {
       b->setPieceAtPosition(move.second, computerChoicePiece);
       b->setPieceAtPosition(move.first, new NullPiece{'*', '*'});
       b->setTurn(human->getPlayerColor());
+      std::cout << b << std::endl;
+      continue;
     }
     // read in stuff
     std::string ask;
