11e2b0a4331eb93e599c0d57a83761f60e369c31
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2022-11-27 22:34:41 -0500

    added Board::makeMove function, implemented (but haven't tested) Computer3

--- src/computer/computer3.h
+++ src/computer/computer3.h
@@ -1,7 +1,18 @@
 #ifndef COMPUTER_THREE_H
 #define COMPUTER_THREE_H
 
+#include <vector>
 
+#include "abstractPlayer.h"
 
+class Computer3 : public AbstractPlayer {
+ public:
+  Computer3(char playerColor, Board *board);
+  ~Computer3() = default;
+
+  // calculate the next move, returns a pair of moves
+  std::pair<std::pair<char, int>, std::pair<char, int>> calculateNextMove()
+      override;
+};
 
 #endif
