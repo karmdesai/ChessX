abf18a8f75ce4c9266e9d5365d8a097dbec53626
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2022-11-29 21:22:52 -0500

    added isComputer field to check when player is computer

--- src/computer/abstractPlayer.h
+++ src/computer/abstractPlayer.h
@@ -8,15 +8,17 @@ class Board;
 class AbstractPlayer {
  protected:
   char playerColor;
+  bool isComputer;
   Board *board;
 
  public:
-  AbstractPlayer(char playerColor, Board *board);
+  AbstractPlayer(char playerColor, Board *board, bool isComputer);
   virtual ~AbstractPlayer() = default;
 
   // Getters
   char getPlayerColor();
   Board *getBoard();
+  bool getIsComputer();
 
   // calculate the next move, returns a pair of moves
   virtual std::pair<std::pair<char, int>, std::pair<char, int>> calculateNextMove() = 0;
