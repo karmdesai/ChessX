1063b7287047ccf12e0b7e2a776e0c8cc7908dc6
Ansh Chaturvedi <anshchaturvedi23@gmail.com>
2022-11-29 22:35:10 -0500

    driver uses updated functions, added inCheck functions

--- src/computer/abstractPlayer.cc
+++ src/computer/abstractPlayer.cc
@@ -3,10 +3,12 @@
 #include "../board/board.h"
 
 // constructor
-AbstractPlayer::AbstractPlayer(char playerColor, Board *board, bool isComputer)
-    : playerColor{playerColor}, board{board}, isComputer{isComputer} {}
+AbstractPlayer::AbstractPlayer(char playerColor, Board *board, bool isAComputer)
+    : playerColor{playerColor}, board{board}, isAComputer{isAComputer} {}
 
 // Getters
 char AbstractPlayer::getPlayerColor() { return this->playerColor; }
 
 Board *AbstractPlayer::getBoard() { return this->board; }
+
+bool AbstractPlayer::isComputer() { return this->isAComputer; }
